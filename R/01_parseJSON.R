0f499cd81e2b630d25927575ec8424c48c1e5907
Jadey Ryan <JRyan@agr.wa.gov>
2022-08-23 14:24:37 -0700

    parse coordinates into long and lat columns

--- R/01_parseJSON.R
+++ R/01_parseJSON.R
@@ -1,8 +1,11 @@
+# import libraries
+
 library(dplyr)
 library(jsonlite)
 library(purrr)
 library(tidyr)
-library(tibble)
+
+# list all files in directory
 
 files <- list.files(
   path = ".\\examples\\examples\\tomkat-historic",
@@ -10,20 +13,61 @@ files <- list.files(
   full.names = TRUE
 )
 
+# import json file
+
 data <- jsonlite::fromJSON(
   txt = files[4],
   flatten = TRUE
 )
 
+# unnest all lists and wrangle data
+
 df <- data$Events %>%
   purrr::map(1) %>%
-  bind_rows(.id = SampleMetaData.ReportID) %>%
-  tidyr::unnest(cols = c(LabMetaData.Reports,
-                  EventSamples.Soil.DepthRefs,
-                  EventSamples.Soil.SoilSamples)) %>%
-  tidyr::unnest(cols = Depths,
-         names_repair = "minimal") %>%
-  tidyr::unnest(cols = NutrientResults,
-         names_repair = "minimal")
+  dplyr::bind_rows(.id = SampleMetaData.ReportID) %>%
+  tidyr::unnest(cols = c(
+    LabMetaData.Reports,
+    EventSamples.Soil.DepthRefs,
+    EventSamples.Soil.SoilSamples
+  )) %>%
+  tidyr::unnest(
+    cols = Depths,
+    names_repair = "minimal"
+  ) %>%
+  tidyr::unnest(
+    cols = NutrientResults,
+    names_repair = "minimal"
+  ) %>%
+  dplyr::rename(
+    EventDate = EventMetaData.EventDate,
+    EventType.Soil = EventMetaData.EventType.Soil,
+    SampleNumber = SampleMetaData.SampleNumber,
+    PointID = SampleMetaData.ReportID,
+    Geometry = SampleMetaData.Geometry.wkt
+  )
 
-#TODO: iterate through all json files in the directory and append to one df
+# parse coordinates into separate columns
+
+df <- df %>%
+  tidyr::separate(Geometry,
+    into = c("long", "lat"),
+    sep = " "
+  )
+
+df$long <- as.numeric(regmatches(
+  df$long,
+  gregexpr("(?>-)*[[:digit:]]+\\.*[[:digit:]]*",
+    df$long,
+    perl = TRUE
+  )
+))
+
+df$lat <- as.numeric(regmatches(
+  df$lat,
+  gregexpr("(?>-)*[[:digit:]]+\\.*[[:digit:]]*",
+    df$lat,
+    perl = TRUE
+  )
+))
+
+# TODO: iterate through all json files in the directory and append to one df
