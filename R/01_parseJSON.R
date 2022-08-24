63b71dad7e524b87f3549b896466fb040fc48af8
Jadey Ryan <JRyan@agr.wa.gov>
2022-08-24 02:29:01 -0700

    add R scripts to process and visualize data, add .qmd to render interactive html doc

--- R/01_parseJSON.R
+++ R/01_parseJSON.R
@@ -4,74 +4,78 @@ library(dplyr)
 library(jsonlite)
 library(purrr)
 library(tidyr)
+library(here)
 
 # list all files in directory
 
 files <- list.files(
-  path = ".\\examples\\examples\\tomkat-historic",
+  path = paste0(here::here(), "/examples/examples/tomkat-historic"),
   pattern = "*.json",
   full.names = TRUE
 )
 
-# import json file
+# import json file from 2015
 
-data <- jsonlite::fromJSON(
+data2015 <- jsonlite::fromJSON(
   txt = files[4],
   flatten = TRUE
 )
 
-# unnest all lists and wrangle data
+# import json file from 2018
 
-df <- data$Events %>%
-  purrr::map(1) %>%
-  dplyr::bind_rows(.id = SampleMetaData.FMISSampleID) %>%
-  tidyr::unnest(cols = c(
-    LabMetaData.Reports,
-    EventSamples.Soil.DepthRefs,
-    EventSamples.Soil.SoilSamples
-  )) %>%
-  tidyr::unnest(
-    cols = Depths,
-    names_repair = "minimal"
-  ) %>%
-  tidyr::unnest(
-    cols = NutrientResults,
-    names_repair = "minimal"
-  ) %>%
-  dplyr::rename(
-    EventDate = EventMetaData.EventDate,
-    EventType.Soil = EventMetaData.EventType.Soil,
-    SampleNumber = SampleMetaData.SampleNumber,
-    FMISSampleID = SampleMetaData.FMISSampleID,
-    Geometry = SampleMetaData.Geometry.wkt
-  )
+data2018 <- jsonlite::fromJSON(
+  txt = files[12],
+  flatten = TRUE
+)
+
+# function to unnest all lists and wrangle data
+
+processJSON <- function(dataFile) {
+  df <- dataFile %>%
+    purrr::map(1) %>%
+    dplyr::bind_rows(.id = SampleMetaData.FMISSampleID) %>%
+    tidyr::unnest(cols = c(
+      LabMetaData.Reports,
+      EventSamples.Soil.DepthRefs,
+      EventSamples.Soil.SoilSamples
+    )) %>%
+    tidyr::unnest(
+      cols = Depths,
+      names_repair = "minimal"
+    ) %>%
+    tidyr::unnest(
+      cols = NutrientResults,
+      names_repair = "minimal"
+    ) %>%
+    dplyr::rename(
+      EventDate = EventMetaData.EventDate,
+      EventType.Soil = EventMetaData.EventType.Soil,
+      SampleNumber = SampleMetaData.SampleNumber,
+      SampleID = SampleMetaData.FMISSampleID,
+      Geometry = SampleMetaData.Geometry.wkt
+    ) %>%
+    # need to replace Bulk Density with BulkDensity for plot function to work
+    dplyr::mutate(
+      Element = ifelse(Element == "Bulk Density",
+        "BulkDensity",
+        Element
+      ),
+      # round to 2 digits
+      Value = round(Value, 2)
+    )
+  return(df)
+}
+
+# read in both files then bind
+
+df2015 <- processJSON(dataFile = data2015$Events)
+
+df2018 <- processJSON(dataFile = data2018$Events)
+
+df <- as.data.frame(rbind(df2015, df2018))
 
 # create unique SampleID from PointID and Date
 
-df$UniqueSampleID <- paste0(df$EventDate, "_", df$FMISSampleID)
-
-# parse coordinates into separate columns
-
-df <- df %>%
-  tidyr::separate(Geometry,
-    into = c("long", "lat"),
-    sep = " "
-  )
-
-df$long <- as.numeric(regmatches(
-  df$long,
-  gregexpr("(?>-)*[[:digit:]]+\\.*[[:digit:]]*",
-    df$long,
-    perl = TRUE
-  )
-))
-
-df$lat <- as.numeric(regmatches(
-  df$lat,
-  gregexpr("(?>-)*[[:digit:]]+\\.*[[:digit:]]*",
-    df$lat,
-    perl = TRUE
-  )
-))
+df$UniqueSampleID <- paste0(df$EventDate, "_", df$SampleID)
 
 # TODO: iterate through all json files in the directory and append to one df
